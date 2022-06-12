904c2449efe161b8fb9439a03253d9aac5cd3133
Mirea Radu <radumirea99@gmail.com>
2022-06-12 20:43:47 +0300

    added embedded image extraction

--- cover_obs.sh
+++ cover_obs.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 COVER="/tmp/album_cover.png"
+EMB_COVER="/tmp/album_cover_embedded.png"
 COVER_SIZE="400"
 
 file="$MUSIC_DIR/$(mpc --format %file% current)"
 album="${file%/*}"
-art=$(find "$album"  -maxdepth 1 | grep -m 1 ".*\.\(jpg\|png\|gif\|bmp\)")
+err=$(ffmpeg -loglevel 16 -y -i "$file" -an -vcodec copy $EMB_COVER 2>&1)
+if [ "$err" != "" ]; then
+  art=$(find "$album"  -maxdepth 1 | grep -m 1 ".*\.\(jpg\|png\|gif\|bmp\)")
+else
+  art=$EMB_COVER
+fi
 if [ "$art" = "" ]; then
   art="$HOME/.ncmpcpp/default_cover.png"
 fi
